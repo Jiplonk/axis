5df2d8e7a933d721f375f47bbb4e9d50efd38ad5
KazuKi <100437797+Jiplonk@users.noreply.github.com>
2022-04-22 23:59:18 +0700

    Add files via upload

--- plugins/Hi.js
+++ plugins/Hi.js
@@ -1,16 +1,16 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'KRIZYN BOTz')).buffer(), `
+let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + '𝙆𝙖𝙯𝙪𝙆𝙞 BOTz')).buffer(), `
 📮 𝘊𝘈𝘙𝘐 𝘈𝘑𝘈 𝘋𝘐 𝘎𝘐𝘛𝘏𝘜𝘉 𝘖𝘙 𝘠𝘖𝘜 𝘛𝘜𝘉𝘌
 ➵͜͡✪ 𝙶𝙸𝚃𝙷𝚄𝙱 :
-github.com/frostkri/krizynv1
+github.com/KazuKi
 ➵͜͡✪ 𝚈𝙾𝚄 𝚃𝚄𝙱𝙴 :
-youtube.com/c/hokenbeusz
+https://youtube.com/channel/UCueojYUvg1lLgdoWeX82UMw
 ➵͜͡✪ 𝙹𝚊𝚗𝚐𝚊𝚗 𝚕𝚞𝚙𝚊 𝚖𝚊𝚖𝚙𝚒𝚛
 https://lynk.id/kri.com
 `.trim(), ' 𝘋𝘰𝘯𝘵 𝘧𝘰𝘳𝘨𝘦𝘵 𝘵𝘰 𝘧𝘰𝘭𝘭𝘰𝘸 𝘮𝘺 𝘢𝘤𝘤𝘰𝘶𝘯𝘵', 'Thanks', '.tqto', 'Menu','.menu')
 handler.help = ['sourcecode']
 -
-`.trim(), '© 𝙆𝙧𝙞𝙯𝙮𝙣×͜×', 'Thanks', '.tqto', 'Back','.menu')
+`.trim(), '© 𝙆𝙖𝙯𝙪𝙆𝙞×͜×', 'Thanks', '.tqto', 'Back','.menu')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
